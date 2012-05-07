da37bda192a41ddd015bf2b7ba767f16e147e22f
koa2000 <konpero@gmail.com>
2012-05-07 18:29:15 +0900

    an interim report

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  force_ssl
+  private
+
+  def current_user
+  	@current_user ||= User.find(session[:user_if]) if session[:user_id]
+  end
+
+  gelper_method :current_users
 end
